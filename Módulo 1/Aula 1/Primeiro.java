4554786e65bc209d5dd364ccc4f27888681cf342
Nicollas Provatti <74670755+nicollas18@users.noreply.github.com>
2024-10-02 13:04:54 -0300

    Add files via upload

---
+++ Módulo 1/Aula 1/Primeiro.java
@@ -0,0 +1,10 @@
+public class Primeiro {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
+
+// A classe Primeiro é o bloco que contém o código
+// O método main é o ponto de entrada do programa, onde a execução começa.
+// String[] args permite receber argumentos da linha de comando.
+// Dentro do método, System.out.println("Olá, mundo!"); é uma instrução que imprimi "Olá, mundo!" na tela.
---
+++ Módulo 1/Aula 1/Segundo.java
@@ -0,0 +1,7 @@
+public class Segundo {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+}
+
+// Exemplo de como usar os argumentos recebidos da linha de comando
